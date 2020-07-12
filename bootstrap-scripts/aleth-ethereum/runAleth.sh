d32502ef06cd13cf0056a5dceae3665e003e6673
liu <liu>
2020-07-12 22:34:51 +0800

    enable test mode withou POW

--- bootstrap-scripts/aleth-ethereum/runAleth.sh
+++ bootstrap-scripts/aleth-ethereum/runAleth.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # copy web3 keys to home directory
+cp -rf keystore ./Ethereum
 cp -rf .web3  ~/
 # run aleth with given configuration
-aleth  --test --config $(pwd)/config.json --password 123456 --db-path $(pwd)/Ethereum --data-dir $(pwd)/Ethereum -m on  -t 6  -a   00eed452f13e237af8089e372c588382990a2df0 --no-discovery --pin --unsafe-transactions
+aleth --config $(pwd)/config.json --master "" --password 123456 --db-path $(pwd)/Ethereum --data-dir $(pwd)/Ethereum -m on -a   00eed452f13e237af8089e372c588382990a2df0 --no-discovery --pin --unsafe-transactions --test
 
