27592bded79e0db2437d28f0db5d757d200544b4
liu <liuye5@live.com>
2020-07-12 20:29:51 +0800

    set --test mode for aleth

--- bootstrap-scripts/aleth-ethereum/runAleth.sh
+++ bootstrap-scripts/aleth-ethereum/runAleth.sh
@@ -2,5 +2,5 @@
 # copy web3 keys to home directory
 cp -rf .web3  ~/
 # run aleth with given configuration
-aleth --config $(pwd)/config.json --password 123456 --db-path $(pwd)/Ethereum --data-dir $(pwd)/Ethereum -m on -a   00eed452f13e237af8089e372c588382990a2df0 --no-discovery --pin --unsafe-transactions
+aleth  --test --config $(pwd)/config.json --password 123456 --db-path $(pwd)/Ethereum --data-dir $(pwd)/Ethereum -m on  -t 6  -a   00eed452f13e237af8089e372c588382990a2df0 --no-discovery --pin --unsafe-transactions
 
