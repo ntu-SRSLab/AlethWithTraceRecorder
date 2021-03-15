d9cc50c80273354de82ed16b2ebe8916f193f8c5
liuye <liuye5@live.com>
2021-03-15 21:59:10 +0800

    save

--- bootstrap-scripts/aleth-ethereum/bootstrap.sh
+++ bootstrap-scripts/aleth-ethereum/bootstrap.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
- python dopple.py  $(pwd)/Ethereum/geth.ipc http://127.0.0.1:8546
+ #python dopple.py  $(pwd)/Ethereum/geth.ipc http://127.0.0.1:8546
+ python3 dopple.py  $(pwd)/Ethereum/geth.ipc http://127.0.0.1:8546
